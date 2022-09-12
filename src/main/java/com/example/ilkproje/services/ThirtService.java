2f2b4094c78def5ecdd3e68c1e64dbd8a1880ae2
Yusuf Emre <yusuf.budak@softtech.com.tr>
2022-09-12 12:30:56 +0300

    thirt commit

--- src/main/java/com/example/ilkproje/services/ThirtService.java
+++ src/main/java/com/example/ilkproje/services/ThirtService.java
@@ -4,6 +4,6 @@ public class ThirtService {
     String a = "string from thirt service and local branch";
 
     String b = "string from thirt service and remote ";
-    String b = "string b from thirt service and local branch";
+    String c = "string b from thirt service and local branch";
 }
 
