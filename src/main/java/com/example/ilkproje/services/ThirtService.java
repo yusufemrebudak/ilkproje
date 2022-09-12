7c479778473be8e4da7b257915dba3a61f893f6f
Yusuf <yusufemrebudak16525@gmail.com>
2022-09-12 12:29:10 +0300

    Update ThirtService.java

--- src/main/java/com/example/ilkproje/services/ThirtService.java
+++ src/main/java/com/example/ilkproje/services/ThirtService.java
@@ -2,5 +2,7 @@ package com.example.ilkproje.services;
 
 public class ThirtService {
     String a = "string from thirt service and local branch";
+    
+    String b = "string from thirt service and remote ";
 }
 
