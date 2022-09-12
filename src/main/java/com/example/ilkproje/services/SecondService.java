90514afe45c16e0ab5a2123c3f88b2f32711b91e
Yusuf Emre <yusuf.budak@softtech.com.tr>
2022-09-12 12:05:15 +0300

    second commit

--- src/main/java/com/example/ilkproje/services/SecondService.java
+++ src/main/java/com/example/ilkproje/services/SecondService.java
@@ -3,4 +3,5 @@ package com.example.ilkproje.services;
 public class SecondService {
     String a = "first commit ilk push eklendi";
     String b = "first commit eklendi";
+    String c = "first commit değil";
  }
